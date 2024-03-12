aab2a1e397f66ba608f729f606e8c7ec9d689f2c
ahtalebi <talebi.phy@gmail.com>
2024-03-12 21:35:23 +0100

    c
    c

--- cleaning_1/Cleaning_Automation_Example.sql
+++ cleaning_1/Cleaning_Automation_Example.sql
@@ -8,17 +8,21 @@ SELECT column_name
 FROM information_schema.columns
 WHERE table_name = 'Cleaning_Automation_Example';
 
-
+--- print number of rows 
 select COUNT(*) as number_ofrows
 from "Cleaning_Automation_Example"
 
----
-	
+--- let's remove empty or 'null' rows and check the new number of rows
 DROP TABLE IF EXISTS New_Cleaning_Automation_Example;
 CREATE TABLE New_Cleaning_Automation_Example AS
 SELECT *
 FROM "Cleaning_Automation_Example"
 WHERE "Equipment Class" IS NOT NULL AND "Equipment Class" != '';
 
+--- removing indentaiton in the new table
+UPDATE "New_Cleaning_Automation_Example"
+SET 
+	"Equipment Class" = TRIM("Equipment Class"),
+    "Department" = TRIM("Department");
 
 SELECT * FROM New_Cleaning_Automation_Example
